c92ee2dd48eefbabb0976acf7763286b5aafe26c
AOB-MAC <aob-mac@AOB-MACui-iMac.local>
2022-11-16 13:52:18 +0900

    input 상태 관리하기

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,52 +1,25 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from 'react';
 
 function InputSample() {
-    const [inputs, setInputs] = useState({
-        name: '',
-        nickname: '',
-    });
-    const nameInput = useRef();
+  const [text, setText] = useState('');
 
-    const { name, nickname } = inputs; //비구조화 할당을 통해 값 추출
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
 
-    const onChange = (e) => {
-        const { value, name } = e.target; // 우선 e.target에서 name과 value를 추출
-        setInputs({
-            ...inputs, // 기존 input 객체를 복사한 뒤
-            [name]: value // name 키를 가진 값을 value로 설정
-        });
-    };
+  const onReset = () => {
+    setText('');
+  };
 
-    const onReset = () => {
-        setInputs({
-            name: '',
-            nickname:'',
-        });
-        nameInput.current.focus();
-    };
-
-    return (
-        <div>
-            <input 
-                name="name" 
-                placeholder="이름" 
-                onChange={onChange} 
-                value={name}
-                ref={nameInput} 
-            />
-            <input 
-                name="nickname" 
-                placeholder="닉네임" 
-                onChange={onChange} 
-                value={nickname} 
-            />
-            <button onClick={onReset}>초기화</button>
-            <div>
-                <b>값 : </b>
-                {name} {(nickname)}
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <input onChange={onChange} value={text}  />
+      <button onClick={onReset}>초기화</button>
+      <div>
+        <b>값: {text}</b>
+      </div>
+    </div>
+  );
 }
 
 export default InputSample;
